9ed29bf554dba8f4cd49198ef04fa776de235ce6
posi7790 <polasp17@gmail.com>
2019-06-30 20:27:44 -0600

    Fix naming button functionality in JS

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-var name = document.getElementById( "strong-dog-name" );
-var nameInput = document.getElementsByClassName( "top-section-article-fieldset-input" );
+window.onload=function(){
+  var name = document.getElementById( "strong-dog-name" );
+  var nameInput = document.querySelector( ".top-section-article-fieldset-input" );
+  var button = document.querySelector( ".top-section-article-fieldset-button" );
 
-document.querySelector( "top-section-article-fieldset-button" ).addEventListener('click', function(event){
-  event.preventDefault();  
-  name.innerHTML = nameInput.value; 
-  nameInput.value = "";
-});
-
+  button.addEventListener('click', function(event){
+    event.preventDefault();  
+    name.innerText = nameInput.value; 
+    nameInput.value = "";
+  });
+}
